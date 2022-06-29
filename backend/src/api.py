238f80cb1eb3cb1361e7bea906b5ca632aefb4f6
Chilex23 <onumaegbuchilex@gmail.com>
2022-06-29 12:52:01 +0100

    Added postman tests to test all the API endpoints

--- backend/src/api.py
+++ backend/src/api.py
@@ -20,14 +20,6 @@ CORS(app)
 #db_drop_and_create_all()
 
 # ROUTES
-'''
-@TODO implement endpoint
-    GET /drinks
-        it should be a public endpoint
-        it should contain only the drink.short() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
-        or appropriate status code indicating reason for failure
-'''
 @app.route("/drinks")
 def get_drinks():
     drinks = Drink.query.all()
@@ -36,14 +28,6 @@ def get_drinks():
         "drinks": [drink.short() for drink in drinks]
     })
 
-'''
-@TODO implement endpoint
-    GET /drinks-detail
-        it should require the 'get:drinks-detail' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
-        or appropriate status code indicating reason for failure
-'''
 @app.route("/drinks-detail")
 @requires_auth("get:drinks-detail")
 def get_drinks_details(payload):
@@ -53,15 +37,6 @@ def get_drinks_details(payload):
         "drinks": [drink.long() for drink in drinks]
     })
 
-'''
-@TODO implement endpoint
-    POST /drinks
-        it should create a new row in the drinks table
-        it should require the 'post:drinks' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
-        or appropriate status code indicating reason for failure
-'''
 @app.route("/drinks", methods=["POST"])
 @requires_auth("post:drinks")
 def create_drink(payload):
@@ -88,17 +63,7 @@ def create_drink(payload):
 
     return jsonify({"success": True, "drinks": [drink.long()]})
 
-'''
-@TODO implement endpoint
-    PATCH /drinks/<id>
-        where <id> is the existing model id
-        it should respond with a 404 error if <id> is not found
-        it should update the corresponding row for <id>
-        it should require the 'patch:drinks' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
-        or appropriate status code indicating reason for failure
-'''
+
 @app.route("/drinks/<id>", methods=["PATCH"])
 @requires_auth("patch:drinks")
 def update_drink(payload, id):
@@ -135,19 +100,6 @@ def update_drink(payload, id):
         "drinks": [drink.long()]
     })
 
-
-
-
-'''
-@TODO implement endpoint
-    DELETE /drinks/<id>
-        where <id> is the existing model id
-        it should respond with a 404 error if <id> is not found
-        it should delete the corresponding row for <id>
-        it should require the 'delete:drinks' permission
-    returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
-        or appropriate status code indicating reason for failure
-'''
 @app.route("/drinks/<id>", methods=["DELETE"])
 @requires_auth("delete:drinks")
 def delete_drinK(payload, id):
@@ -171,6 +123,13 @@ def delete_drinK(payload, id):
 '''
 Example error handling for unprocessable entity
 '''
+@app.errorhandler(401)
+def unauthorized(error):
+    return jsonify({
+        "success": False,
+        "error": 401,
+        "message": "Unauthorized"
+    }), 401
 
 
 @app.errorhandler(422)
@@ -181,17 +140,6 @@ def unprocessable(error):
         "message": "unprocessable"
     }), 422
 
-
-'''
-@TODO implement error handlers using the @app.errorhandler(error) decorator
-    each error handler should return (with approprate messages):
-             jsonify({
-                    "success": False,
-                    "error": 404,
-                    "message": "resource not found"
-                    }), 404
-
-'''
 @app.errorhandler(400)
 def bad_request(error):
     return jsonify({
@@ -205,7 +153,7 @@ def unauthorized(error):
     return jsonify({
         "success": False,
         "error": 403,
-        "message": "Unauthorized access."
+        "message": "You are forbidden from making this request."
     }), 403
 
 @app.errorhandler(409)
@@ -213,7 +161,7 @@ def conflict(error):
     return jsonify({
         "success": False,
         "error": 409,
-        "message": "A conflict was found."
+        "message": "Another drink with this name already exists."
     }), 409
 
 
